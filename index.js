d0c26735e9e1450d20a832aa11e922de8b50f413
Dariush Alipour <drsh.alipour@gmail.com>
2017-04-30 17:33:46 +0430

    made es5-friendly

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable object-shorthand */
 const EventManager = require('./src/event.manager.js').EventManager;
 
 const App = function App(config = {}) {
@@ -9,34 +10,66 @@ const App = function App(config = {}) {
     },
   });
 
-  const getAdaptersInfo = () => eventManager.getAdaptersInfo();
-  const setPrimaryAdapterId = adapterId => new App({
-    primaryAdapterId: adapterId,
-    adapters: config.adapters,
-    generators: config.generators,
-  });
-  const getGeneratorsInfo = () => eventManager.getGeneratorsInfo();
-  const generateDate = dateConfig => eventManager.generateDate(dateConfig);
-  const getCurrentDate = () => eventManager.getCurrentDate();
-  const addEvents = eventConfigs => eventConfigs.reduce((acc, cfg) => eventManager.addEvent(cfg));
-  const getMonthInfo = (year, month) => eventManager.getMonthInfo(year, month);
-  const getEventsIn = (since, till) => eventManager.getEventsIn(since, till);
-  const addEvent = eventConfig => eventManager.addEvent(eventConfig);
-  const removeEvent = eventId => eventManager.removeEvent(eventId);
-  const editEvent = eventConfig => eventManager.editEvent(eventConfig);
+  const getAdaptersInfo = function getAdaptersInfo() {
+    return eventManager.getAdaptersInfo();
+  };
+
+  const setPrimaryAdapterId = function setPrimaryAdapterId(adapterId) {
+    return new App({
+      primaryAdapterId: adapterId,
+      adapters: config.adapters,
+      generators: config.generators,
+    });
+  };
+
+  const getGeneratorsInfo = function getGeneratorsInfo() {
+    return eventManager.getGeneratorsInfo();
+  };
+
+  const generateDate = function generateDate(dateConfig) {
+    return eventManager.generateDate(dateConfig);
+  };
+
+  const getCurrentDate = function getCurrentDate() {
+    return eventManager.getCurrentDate();
+  };
+
+  const addEvents = function addEvents(eventConfigs) {
+    return eventConfigs.reduce((acc, cfg) => eventManager.addEvent(cfg));
+  };
+
+  const getMonthInfo = function getMonthInfo(year, month) {
+    return eventManager.getMonthInfo(year, month);
+  };
+
+  const getEventsIn = function getEventsIn(since, till) {
+    return eventManager.getEventsIn(since, till);
+  };
+
+  const addEvent = function addEvent(eventConfig) {
+    return eventManager.addEvent(eventConfig);
+  };
+
+  const removeEvent = function removeEvent(eventId) {
+    return eventManager.removeEvent(eventId);
+  };
+
+  const editEvent = function editEvent(eventConfig) {
+    return eventManager.editEvent(eventConfig);
+  };
 
   return {
-    getAdaptersInfo,
-    setPrimaryAdapterId,
-    getGeneratorsInfo,
-    generateDate,
-    getCurrentDate,
-    addEvents,
-    getMonthInfo,
-    getEventsIn,
-    addEvent,
-    removeEvent,
-    editEvent,
+    getAdaptersInfo: getAdaptersInfo,
+    setPrimaryAdapterId: setPrimaryAdapterId,
+    getGeneratorsInfo: getGeneratorsInfo,
+    generateDate: generateDate,
+    getCurrentDate: getCurrentDate,
+    addEvents: addEvents,
+    getMonthInfo: getMonthInfo,
+    getEventsIn: getEventsIn,
+    addEvent: addEvent,
+    removeEvent: removeEvent,
+    editEvent: editEvent,
   };
 };
 
